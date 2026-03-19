f42a46be86bbb3a995285bbf60209135630cf355
MINJUN17 <71377165+MINJUN17@users.noreply.github.com>
2026-03-19 17:42:20 +0900

    샷건 제작 중

---
+++ ConsoleApp1/Shooting/Weapons/ShotGun.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class ShotGun : Weapon
+{
+    public ShotGun()
+    {
+        _cooldown = 1.0f;
+    }
+    public override void Fire(PlayScene scene, Position pos, Direction dir)
+    {
+        if(dir == Direction.Up)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir + 2);
+            scene.CreateBullet(pos, dir + 3);
+        }
+        if(dir == Direction.Down)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir + 3);
+            scene.CreateBullet(pos, dir + 4);
+        }
+        if (dir == Direction.Left)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir + 4);
+            scene.CreateBullet(pos, dir + 6);
+        }
+        if (dir == Direction.Right)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir + 4);
+            scene.CreateBullet(pos, dir + 6);
+        }
+        if (dir == Direction.DownLeft)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir - 6);
+            scene.CreateBullet(pos, dir - 3);
+        }
+        if (dir == Direction.DownRight)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir - 4);
+            scene.CreateBullet(pos, dir - 6);
+        }
+        if (dir == Direction.UpRight)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir - 3);
+            scene.CreateBullet(pos, dir - 4);
+        }
+        if (dir == Direction.UpLeft)
+        {
+            scene.CreateBullet(pos, dir);
+            scene.CreateBullet(pos, dir - 2);
+            scene.CreateBullet(pos, dir - 4);
+        }
+    }
+}
